d36b93ab652d0a897f75b21bf00ef3d69f6cb253
SCsiz <stevenj005@yahoo.com>
2017-07-30 13:16:15 -0400

    Add files via upload

---
+++ frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -0,0 +1,336 @@
+ var bio = {
+	"name": "James T Kirk",
+	"role": "Captain",
+	"contacts": {
+		"email": "Captain_1701@starfleet.com",
+		"github": "captain_1701",
+		"twitter": "@captain_1701",
+		"location": "among the stars"
+	},
+	"picture": "images/me.png",
+	"welcomeMessage": "Original captain of the starship Enterprise, boldy going where no one has gone before. Way better than Picard and Spock, everyone says so",
+	"skills": ["Captaining", "Green women", "Overacting", "Outlasting shipmates", "Hotel, plane, and cruise savings"]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "Starfleet",
+			"title": "StarFleet Officer - Captain",
+			"location": "San Francisco, Ca",
+			"datesWorked": "2254 - 2371",
+			"description": "Captain of the starship Enterprise"
+		},
+		{
+			"employer": "none - retired/dead",
+			"title": "Stuck in the Nexus",
+			"location": "Nexus",
+			"datesWorked": "2371 - present",
+			"description": "I was commissioning the Enterprise B on its maiden voyage when an energy anomoly appeared. We intercepted and in trying to save some of the crew, I was " +
+				"swallowed into the Nexus rift where I have been shopping wood at my old home"
+		}
+		
+	]
+};
+
+var education = {
+	"schools": [
+		{ "name": "Starfleet Academy",
+			"datesAttended": "2250 - 2254",
+			"location": "Fort Baker, Ca",
+			"degree": "Captain",
+			"major": "Starfleet Officer",
+			"url": "www.Starfleet.com"
+		}		
+	],
+	"onlineCourses": [
+		{ "school": "Udacity",
+			"title": "Intro to Programming",
+			"completed": "2016 - 2017",
+			"url": "https://www.udacity.com"
+		}
+	]
+};
+
+var projects = {
+	"projects": [
+		{
+			"title": "HTML5 Canvas Game",
+			"datesWorked": "December 2014 - January 2015",
+			"description": "Created an online game using HTML5 Canvas as part of Udacity's Front-End Web Developer " +
+			"Nanodegree.",
+			"images": ["images/frogger.jpg"],
+			"url": "http://www.cherylcourt.ca/frogger"
+		},
+		{
+			"title": "Online Portfolio",
+			"datesWorked": "October 2014",
+			"description": "Created an online portfolio of work as part of Udacity's Front-End Web Developer " +
+			"Nanodegree.",
+			"images": ["images/portfolio.jpg"],
+			"url": "http://www.cherylcourt.ca/"
+		},
+		{
+			"title": "Web Development Wiki",
+			"datesWorked": "June 2014 - August 2014",
+			"description": "Created an online wiki for Udacity's Web Development Course.",
+			"images": ["images/web.jpg"],
+			"url": "http://helpful-kit-572.appspot.com/"
+		}
+	]
+};
+
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picture);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+if(bio.skills.length > 0) {
+	$("#header").append(HTMLskillsStart);
+
+	for(i in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+	}
+}
+
+for(i in formattedContactInfo) {
+	$("#topContacts").append(formattedContactInfo[i]);
+	$("#footerContacts").append(formattedContactInfo[i]);
+}
+
+
+function displayWork() {
+
+	if(work.jobs.length > 0) {
+	
+		$("#workExperience").append(HTMLworkStart);
+
+		for(i in work.jobs) {
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[i].location);
+			var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[i].datesWorked);
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[i].description);
+
+			var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+
+			$(".work-entry:last").append(formattedEmployerWorkTitle);
+			$(".work-entry:last").append(formattedWorkLocation);
+			$(".work-entry:last").append(formattedDatesWorked);
+			$(".work-entry:last").append(formattedWorkDescription);
+		}
+
+	}
+
+}
+
+displayWork();
+
+
+projects.display = function() {
+	if(projects.projects.length > 0) {
+		for(i in projects.projects) {
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title).replace("#", projects.projects[i].url);
+			var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].datesWorked);
+			var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+
+			$(".project-entry:last").append(formattedProjectTitle);
+			$(".project-entry:last").append(formattedProjectDates);
+			$(".project-entry:last").append(formattedProjectDescription);
+
+			for(img in projects.projects[i].images) {
+				var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[i].images[img]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+			
+
+		}
+	}
+}
+
+projects.display();
+
+education.display = function() {
+	if(education.schools.length > 0 || education.onlineCourses.length > 0) {
+		for(i in education.schools) {
+			$("#education").append(HTMLschoolStart);
+
+			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name).replace("#", education.schools[i].url);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].datesAttended);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);			
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].major);
+			var formattedSchoolMinor = HTMLschoolMinor.replace("%data%", education.schools[i].minor);
+
+			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+			$(".education-entry:last").append(formattedSchoolDates);
+			$(".education-entry:last").append(formattedSchoolLocation);
+			$(".education-entry:last").append(formattedSchoolMajor);
+			$(".education-entry:last").append(formattedSchoolMinor);
+		}
+
+		if(education.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for(i in education.onlineCourses) {				
+				$("#education").append(HTMLschoolStart);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title).replace("#", education.onlineCourses[i].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].completed);
+				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url).replace("#", education.onlineCourses[i].url);
+
+				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+				$(".education-entry:last").append(formattedOnlineDates);
+				$(".education-entry:last").append(formattedOnlineURL);
+			}
+		}
+		
+	}
+}
+
+education.display();
+
+/**
+ * Skills Chart
+ */
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'scatter',
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Skills'
+        },
+        xAxis: {
+            title: {
+                enabled: true,
+                text: 'Used (year)'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true
+        },
+        yAxis: {
+            title: {
+                text: 'Duration (months)'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 250,
+            y: 0,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF',
+            borderWidth: 1
+        },
+        plotOptions: {
+            scatter: {
+                marker: {
+                    radius: 5,
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineColor: 'rgb(100,100,100)'
+                        }
+                    }
+                },
+                states: {
+                    hover: {
+                        marker: {
+                            enabled: false
+                        }
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<b>{series.name}</b><br>',
+                    pointFormat: 'Used in {point.x} for {point.y} month(s)'
+                }
+			}
+		},
+		series: [{
+				name: 'Ruby',
+				color: 'rgba(255, 0, 0, .5)',
+				data: [[2017, 4], [2016, 12]]
+			},
+			{
+				name: 'Javascript',
+				color: 'rgba(204, 0, 0, .5)',
+				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 1]]
+			},
+			{
+				name: 'HTML/CSS',
+				color: 'rgba(153, 51, 0, .5)',
+				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 5], [2002, 3], [2008, 4], [2005, 2]]
+			},
+			{
+				name: 'Web Development',
+				color: 'rgba(0, 153, 102, .5)',
+				data: [[2017, 4], [2016, 12], [2015, 8], [2014, 2]]
+			},
+			{
+				name: 'TDD',
+				color: 'rgba(0, 102, 51, .5)',
+				data: [[2016, 12], [2014, 12], [2013, 12], [2012, 12], [2011, 4]]
+			},
+			{
+				name: 'Java',
+				color: 'rgba(223, 83, 83, .5)',
+				data: [[2014, 11], [2013, 11], [2012, 11], [2011, 8], [2010, 4], [2009, 12], [1998, 8], [1999, 8], [2000, 4]]
+			},
+			{
+				name: 'python',
+				color: 'rgba(119, 152, 191, .5)',
+				data: [[2014, 1], [2013, 1], [2012, 1], [2011, 1]]
+			},
+			{
+				name: 'SQL',
+				color: 'rgba(119, 152, 80, .5)',
+				data: [[2016, 12], [2013, 2]]
+			},
+			{
+				name: 'PL/SQL',
+				color: 'rgba(170, 162, 30, .5)',
+				data: [[2010, 2], [2006, 4]]
+			},
+			{
+				name: 'C++',
+				color: 'rgba(0, 102, 0, .5)',
+				data: [[2004, 12], [2003, 6]]
+			},
+			{
+				name: 'C',
+				color: 'rgba(0, 102, 0, .5)',
+				data: [[1997, 8], [2004, 2], [2008, 4]]
+			},
+			{
+				name: 'Django',
+				color: 'rgba(204, 102, 0, .5)',
+				data: [[2013, 2.5]]
+			},
+			{
+				name: 'Perl',
+				color: 'rgba(210, 200, 0, .5)',
+				data: [[2011, 3], [2013, 1], [2012, 1], [2014, 1]]
+			}]
+	});
+});
+
+
+$("#mapDiv").append(googleMap);
